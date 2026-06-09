af59e0e22f022d7bebaaedb52adfb0cd524c0a6a
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-09 22:05:49 +0530

    Implement string compression in CompressString.java

---
+++ Strings/CompressString.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        StringBuilder res=new StringBuilder();
+        int count=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                count++;
+            }else{
+                res.append(s.charAt(i-1)).append(count);
+                count=1;
+            }
+        }
+        res.append(s.charAt(s.length() -1 )).append(count);
+        System.out.println(res);
+    }
+}
