eb9ba6d693d0d5c1844e8fa15cc1992eac75b12b
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-09 22:08:19 +0530

    Refactor main method in DigitString.java
    Removed placeholder comment and added input handling.

---
+++ Strings/DigitString.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        if(isDigit)
+        System.out.println("Yes");
+        else
+        System.out.println("No");*/
+        if(s.matches("[0-9]+"))
+        System.out.println("Yes");
+        else
+        System.out.println("No");
+    }
+}
