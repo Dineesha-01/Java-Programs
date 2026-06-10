c3e50785377674718c781ae935394be685755ef8
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-10 23:05:41 +0530

    Added MaxAltitude.java to calculate maximum altitude

---
+++ Arrays/MaxAltitude.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int c=0,m=0;
+        for(int i=0;i<n;i++){
+            c+=a[i];
+            if(c>m){
+                m=c;
+            }
+        }
+        System.out.println(m);
+    }
+}
