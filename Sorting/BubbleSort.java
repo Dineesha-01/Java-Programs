cb7f8c7a1ce7b5fe5ef5a931f3b2a4ed6e2a785f
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-11 20:31:59 +0530

    Add BubbleSort implementation in BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+            for(int k=0;k<n;k++){
+                System.out.print(a[k] +" ");
+            }
+            System.out.println();
+        }
+    }
+}
