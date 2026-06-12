0db13b6a7627f90fa141cd9c8b12bde0fbeac3d9
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-12 20:51:54 +0530

    Clean up main method in ShellSort.java
    Removed placeholder comment in main method.

---
+++ Sorting/ShellSort.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int gap=n/2;gap>0;gap=gap/2){
+            for(int i=gap;i<n;i++){
+                int temp=a[i];
+                int j;
+                for(j=i;j>=gap && a[j-gap]>temp;j=j-gap){
+                    a[j]=a[j-gap];
+                }
+                a[j]=temp;
+            }
+            for(int i=0;i<n;i++){
+                System.out.print(a[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
