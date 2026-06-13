049c69349e85d177312cb5a7cd3a95d211b75c39
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-13 20:07:30 +0530

    Add MatrixColumnSum.java for column sum calculation
    Implement a program to calculate the sum of each column in a matrix.

---
+++ Matrix/MatrixColumnSum.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int [][]matrix=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        for(int j=0;j<m;j++){
+            int sum=0;
+            for(int i=0;i<n;i++){
+                sum+=matrix[i][j];
+            }
+            System.out.println(sum);
+        }
+    }
+}
