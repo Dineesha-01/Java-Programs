77a3b29a537bd93796249d63d2044b1c55da72e5
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-17 21:39:48 +0530

    Add ArrangePrimes.java to calculate prime arrangements

---
+++ Arrays/ArrangePrimes.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+        static final long Mod=1000000007;
+        static boolean isPrime(int n){
+            if(n<2) return false;
+            for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+        static long fact(int n){
+            long ans=1;
+            for(int i=2;i<=n;i++)
+             ans=(ans*i)%Mod;
+            return ans;
+        }
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int primecount=0;
+            for(int i=1;i<=n;i++){
+                if(isPrime(i)){
+                    primecount++;
+                }
+            }
+            int nonPrime = n-primecount;
+            long result=(fact(primecount)*fact(nonPrime))%Mod;
+            System.out.println(result);
+    }
+}
