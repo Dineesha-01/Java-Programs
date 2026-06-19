b4ff3ba7220bac864727009ce56efb62eeacc425
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-19 20:45:49 +0530

    Add Gauntlets.java to count color pairs
    You have a collection of N gauntlets, each with a specific color represented by A[i]. Your goal is to maximize the number of pairs by repeatedly pairing up gauntlets of the same color. Determine the maximum number of pairs that can be formed.
    
    Input Format
    The first line of input contains an integer N. The second line of input contains an array of size N.
    
    Output Format
    For the given input, print a single line representing the answer.
    
    Constraints
    1 ≤ N ≤ 102
    1 ≤ Ai ≤ 103
    
    Example
    Input
    6
    4 1 7 4 1 4
    
    Output
    2

---
+++ Arrays/Gauntlets.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] f=new int[1001];
+        for(int i=0;i<n;i++){
+            int color=sc.nextInt();
+            f[color]++;
+        }
+        int  pairs=0;
+        for(int i=0;i<=1000;i++){
+            pairs+=f[i]/2;
+        }
+        System.out.println(pairs);
+        sc.close();
+    }
+}
