4e3a92a1a6639c06113cae9002bd28ae013c0f2d
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-23 20:53:10 +0530

    Clean up main method in ArithmeticProgression.java
    Removed placeholder comment in main method.

---
+++ Math/ArithmeticProgression.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int d=sc.nextInt();
+        int n=sc.nextInt();
+        int x=a + (n-1) * d;
+        System.out.println(x);
+    }
+}
