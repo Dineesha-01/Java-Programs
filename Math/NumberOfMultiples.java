41ba667cb6c6598cb16c418b6902939b0913940b
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-23 20:55:47 +0530

    Add NumberOfMultiples.java to calculate multiples
    Implement a program to calculate the number of multiples of 3 and 5 up to n.

---
+++ Math/NumberOfMultiples.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        long total=(n/3)+(n/5)-(n/15);
+        System.out.println(total);
+    }
+}
