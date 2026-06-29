afdb819f9e8db92fc2ab7fa31c471064ca3a93c4
Dineesha-01 <23BQ1A1214@vvit.net>
2026-06-29 20:14:27 +0530

    Add MultiplicationTable.java for multiplication output

---
+++ Math/MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" * "+i+" = "+(n*i));
+        }
+    }
+}
